77bd8fad5587a5374355ab6acab193a9210ff37a
Rodrigo Silva Mendoza <rodrigo.silvamendoza@datadoghq.com>
2026-03-09 18:58:44 +0000

    bump to latest

--- rdkafka-sys/src/bindings.rs
+++ rdkafka-sys/src/bindings.rs
@@ -52,8 +52,6 @@ pub const RD_KAFKA_EVENT_DESCRIBETOPICS_RESULT: i32 = 1048576;
 pub const RD_KAFKA_EVENT_DESCRIBECLUSTER_RESULT: i32 = 2097152;
 pub const RD_KAFKA_EVENT_LISTOFFSETS_RESULT: i32 = 4194304;
 pub const RD_KAFKA_EVENT_ELECTLEADERS_RESULT: i32 = 8388608;
-pub const RD_KAFKA_BROKER_STATE_COUNT: i32 = 8;
-pub const RD_KAFKA_FETCH_STATE_COUNT: i32 = 6;
 unsafe extern "C" {
     pub fn rd_kafka_version() -> c_int;
 }
@@ -181,6 +179,8 @@ pub struct rd_kafka_broker_stats_s {
     pub waitresp_cnt: i32,
     pub waitresp_msg_cnt: i32,
     pub tx: i64,
+    pub tx_iovecs: i64,
+    pub tx_multiseg: i64,
     pub tx_bytes: i64,
     pub tx_errs: i64,
     pub tx_retries: i64,
@@ -305,12 +305,6 @@ pub type rd_kafka_stats_t = rd_kafka_stats_s;
 #[derive(Debug, Copy, Clone)]
 pub struct rd_kafka_s {
     _unused: [u8; 0],
-}
-unsafe extern "C" {
-    pub fn rd_kafka_stats_new(rk: *mut rd_kafka_s) -> *mut rd_kafka_stats_t;
-}
-unsafe extern "C" {
-    pub fn rd_kafka_stats_destroy(stats: *mut rd_kafka_stats_t);
 }
 pub type rd_kafka_t = rd_kafka_s;
 #[repr(C)]
@@ -2122,12 +2116,6 @@ unsafe extern "C" {
 }
 unsafe extern "C" {
     pub fn rd_kafka_event_stats_typed(rkev: *mut rd_kafka_event_t) -> *const rd_kafka_stats_t;
-}
-unsafe extern "C" {
-    pub static mut rd_kafka_broker_state_names: [*const c_char; 0usize];
-}
-unsafe extern "C" {
-    pub static mut rd_kafka_fetch_states: [*const c_char; 0usize];
 }
 unsafe extern "C" {
     pub fn rd_kafka_event_topic_partition_list(
